588e2b478cd4d7562f96edfeffddb2a9bf83e817
vincent-k-sm <kimtmdals@gmail.com>
2023-03-21 06:36:41 +0900

    remove resource dir from navigator

---
+++ Resources/Package.swift
@@ -0,0 +1,6 @@
+
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// DUMMY FILE FOR REMOVE RESOURCE DIR
+import PackageDescription
